1637101438d9ac2c2b3872e7e1ab4fbb815427ca
sjoerdwol <sjoerd@woltersonline.net>
2023-01-07 16:45:28 +0100

    started implementing Redux content

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -4,7 +4,8 @@ const nav_html = document.getElementById('nav_html');
 const nav_css = document.getElementById('nav_css');
 const nav_js = document.getElementById('nav_js');
 const nav_react = document.getElementById('nav_react');
-const nav_elements = [nav_home, nav_html, nav_css, nav_js, nav_react];
+const nav_redux = document.getElementById('nav_redux');
+const nav_elements = [nav_home, nav_html, nav_css, nav_js, nav_react, nav_redux];
 const sections = document.getElementsByTagName('section');
 
 //ASSIGNMENTS
@@ -36,6 +37,9 @@ function navOnClick(event) {
   } else if(event.target === nav_react) {
     sections[4].classList.remove('hidden');
     console.log('Now showing React Screen!');
+  } else if(event.target === nav_redux) {
+    sections[5].classList.remove('hidden');
+    console.log('Now showing Redux Screen!');
   } else {
     console.log('Something weird happened: ' + event.target);
   };
